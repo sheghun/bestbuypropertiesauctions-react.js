20663ca385b2d8058f0e75c3753632c233e14b7d
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2019-12-17 15:22:22 +0100

    Live api works for the user end

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,7 +2,6 @@
 // import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http:/' +
-    '/localhost:8080';
-// axios.defaults.baseURL = 'https://api.bestbuypropertiesauctions.com';
+// axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://api.bestbuypropertiesauctions.com';
 axios.defaults.withCredentials = true;
